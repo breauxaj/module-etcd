9cb7ba486eb8acaf83bd4ba5394bfc85e258e622
Adam Breaux <breauxaj@gmail.com>
2016-11-02 17:39:27 -0400

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,42 @@
+# Class: etcd
+#
+# This class installs etcd
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine which version to download
+#
+# Actions:
+#   - Install etcd
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'etcd': }
+#
+class etcd (
+  $ensure = $::etcd::params::etcd_package_ensure
+) inherits ::etcd::params {
+  case $::osfamily {
+    'RedHat': {
+      package { $::etcd::params::etcd_package:
+        ensure  => $ensure,
+      }
+
+      $config = hiera('etcd',{})
+      create_resources('etcd::config',$config)
+
+      service { $::etcd::params::etcd_service:
+        ensure  => running,
+        enable  => true,
+        require => Package[$::etcd::params::etcd_package],
+      }
+    }
+    default: {
+      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
+    }
+  }
+
+}
